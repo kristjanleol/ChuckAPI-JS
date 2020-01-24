d17ce86a02018277dc9d92f3601410d6d29d8615
kristjanleol <kristjan.leol@gmail.com>
2020-01-24 10:51:24 +0200

    second commit

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(event){
     const userNumber = document.querySelector('input[type="number"]').value;
+    //add proxy
+    const proxyrl = "https://cors-anywhere.herokuapp.com/";
     //console.log(typeof(userNumber));
 
     //GET request
-
-    fetch(`http://api.icndb.com/jokes/random/${userNumber}`)
+    
+    fetch(`${proxyUrl}http://api.icndb.com/jokes/random/${userNumber}`)
     .then(function(response){
         console.log(response);
         return response.json();
