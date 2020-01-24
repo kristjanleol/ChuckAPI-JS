5e35d0dfe01d99349bd12589b82c5b729cd0dcd7
kristjanleol <kristjan.leol@gmail.com>
2020-01-24 10:32:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//Listen to the click event on the get-jokes button
+
+//select the button, add the event listener
+
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(event){
+    const userNumber = document.querySelector('input[type="number"]').value;
+    //console.log(typeof(userNumber));
+
+    //GET request
+
+    fetch(`http://api.icndb.com/jokes/random/${userNumber}`)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+       
+    })
+    .then(function(data){
+        let output = '';
+        //Each joke in data.value array
+        data.value.forEach(joke => {
+            output += `<li>${joke.joke}</li>`
+            console.log(joke.joke);    
+        });
+        document.querySelector('.jokes').innerHTML = output;
+    });
+     
+    event.preventDefault();
+}
